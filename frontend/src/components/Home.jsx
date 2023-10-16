fc6d3d63ae1ef153f310ec04b3ad1e868c2087aa
logos722 <logos535@gmail.com>
2023-10-16 22:26:07 +0300

    semilocons...

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ChannelList from './ChannelList.jsx';
 import MessageList from './MessageList.jsx';
 
-const Home = () => {
+const Home = () => (
   <div className="container h-100 my-4 overflow-hidden rounded shadow">
     <div className="row h-100 bg-white flex-md-row">
       <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
@@ -12,7 +12,7 @@ const Home = () => {
         <MessageList />
       </div>
     </div>
-  </div>;
-};
+  </div>
+);
 
 export default Home;
