c3c993a12a0b50bfa25340db1afe3b352f7b0aad
Vladislav Kostyuk <logos535@gmail.com>
2023-11-03 21:01:40 +0300

    lint fix

--- frontend/src/components/modal/ModalRename.jsx
+++ frontend/src/components/modal/ModalRename.jsx
@@ -33,6 +33,7 @@ const ModalRename = () => {
   useEffect(() => {
     setChannelName(channelBeingEdited.name);
     inputRef.current.focus();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const resolve = (id) => {
